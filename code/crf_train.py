aff779b821c6e8945806588165abff3df4492087
S.Li <lisheng@patsnap.com>
2017-01-13 16:52:13 +0800

    Add get_tc_sn_type to merge factset data

--- code/crf_train.py
+++ code/crf_train.py
@@ -4,17 +4,18 @@ from .arsenal_stats import *
 import spacy
 
 
-HEADER_FS = ['"_id"', 'entity_name', 'entity_proper_name', 'entity_sub_type', 'entity_type']
+HEADER_FS = ['fact', 'entity_proper_name', 'entity_type']
+HEADER_SN = ['factset_entity_id', 'short_name']
 HEADER_TC = ['"ID"', '"TITLE"', '"CONTENT"', '"TIME"']
 NER_LABEL = ('PERSON', 'NORP', 'ORG', 'GPE', 'PRODUCT', 'EVENT', 'MONEY')
 NLP = spacy.load('en')
 
 
-def read_faceset(in_file):
+def read_faceset(in_file, column_names):
     """
     param: in_file: csv file
     """
-    data = csv2pd(column_constant=get_header(in_file), column_names=HEADER_FS, engine='c',
+    data = csv2pd(column_constant=get_header(in_file), column_names=column_names, engine='c',
                   in_file=in_file, quote=0, sep=',')
     return data
 
@@ -40,6 +41,22 @@ def spacy_ner(sent):
     return extracted
 
 
+# def df2gold_parser(df, entity_col='entity_proper_name', tag_col='entity_type'):
+#     length = df[entity_col].str.len()
+#     tag = df[tag_col]
+#     zip(len_list)
+
+
+##############################################################################################
+
+
 def batch_processing(in_file, col='CONTENT'):
     data = read_techcrunch(in_file)
     return data[col].apply(spacy_ner)
+
+
+def get_factset_sn_type(type_file, sn_file):
+    sn = read_faceset(sn_file, HEADER_SN)
+    ty = read_faceset(type_file, HEADER_FS)
+    result = pd.merge(ty, sn, on='factset_entity_id', how='inner')
+    return result.dropna()
