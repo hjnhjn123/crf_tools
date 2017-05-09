09e6daa0c69838d2571f1b3489c77a8c5e44201c
SLi <sheng.li@gemii.cc>
2017-05-09 18:05:57 +0800

    Mv crf_result2dict, extract_ner_result

--- code/pipeline_crf.py
+++ code/pipeline_crf.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from collections import Counter
-from copy import deepcopy
+from itertools import groupby
 from math import modf
 from os import listdir, path
-from itertools import groupby
 
 import joblib as jl
 import redis
@@ -14,34 +12,11 @@ from arsenal_logging import *
 from arsenal_spacy import *
 from arsenal_stats import *
 
-STOP_ROOTS = {'is', 'are', 'was', 'were', 'been', 'be'}
-HEADER_ANNOTATION = ['TOKEN' 'POS', 'NER']
 HDF_KEY_20170425 = ['aca', 'com_single', 'com_suffix', 'location', 'name', 'ticker',
-                    'tfdf',
-                    'tfidf']
+                    'tfdf', 'tfidf']
 
 
 ##############################################################################
-
-
-def process_annotated(in_file):
-    """
-    | following python-crfsuit, sklearn_crfsuit doesn't support pandas DF, so a feature 
-    | dic is used instead
-    | http://python-crfsuite.readthedocs.io/en/latest/pycrfsuite.html#pycrfsuite.ItemSequence
-    :param in_file: CSV file: TOKEN, POS, NER
-    :return: [[sent]]
-    """
-    data = pd.read_csv(in_file, header=None, engine='c', quoting=0)
-    data.columns = HEADER_ANNOTATION
-    data = data.dropna()
-    sents = (tuple(i) for i in
-             zip(data['TOKEN'].tolist(), data['POS'].tolist(), data['NER'].tolist()))
-    sents = (list(x[1])[:-1] for x in groupby(sents, lambda x: x == ('##END', '###', 'O'))
-             if
-             not x[0])
-    sents = [i for i in sents if i != []]
-    return sents
 
 
 def prepare_features(aca_df, com_single_df, com_suffix_df, location_df, name_df,
@@ -49,49 +24,27 @@ def prepare_features(aca_df, com_single_df, com_suffix_df, location_df, name_df,
     aca, com_single = df2set(aca_df), df2set(com_single_df)
     name, location = df2set(name_df), df2set(location_df)
     ticker = df2set(ticker_df)
-    com_suffix = {i.title() for i in df2set(com_suffix_df)}
-    tfdf = {k: v for (k, v) in zip(tfdf_df.Token, tfdf_df.Value)}
-    tfidf = {k: v for (k, v) in zip(tfidf_df.Token, tfidf_df.Value)}
+    com_suffix = df2set(com_suffix_df, title=True)
+    tfdf = df2dic(tfdf_df)
+    tfidf = df2dic(tfidf_df)
     return aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf
 
 
 def batch_add_features(pos_data, aca, com_single, com_suffix, location, name, ticker,
                        tfdf, tfidf):
-    added_aca = (map_list_2_matrix(chunk, aca) for chunk in pos_data)
-    added_com_single = (map_list_2_matrix(chunk, com_single) for chunk in added_aca)
-    added_com_suffix = (map_list_2_matrix(chunk, com_suffix) for chunk in
+    added_aca = (map_set_2_matrix(chunk, aca) for chunk in pos_data)
+    added_com_single = (map_set_2_matrix(chunk, com_single) for chunk in added_aca)
+    added_com_suffix = (map_set_2_matrix(chunk, com_suffix) for chunk in
                         added_com_single)
-    added_location = (map_list_2_matrix(chunk, location) for chunk in added_com_suffix)
-    added_name = (map_list_2_matrix(chunk, name) for chunk in added_location)
-    added_ticker = (map_list_2_matrix(chunk, name) for chunk in added_name)
+    added_location = (map_set_2_matrix(chunk, location) for chunk in added_com_suffix)
+    added_name = (map_set_2_matrix(chunk, name) for chunk in added_location)
+    added_ticker = (map_set_2_matrix(chunk, ticker) for chunk in added_name)
     added_tfidf = (map_dict_2_matrix(chunk, tfidf) for chunk in added_ticker)
     result = [map_dict_2_matrix(chunk, tfdf) for chunk in added_tfidf]
     return result
 
 
-def crf_result2dict(crf_result):
-    ner_candidate = [(token, ner) for token, _, ner in crf_result if ner[0] != 'O']
-    ner_index = (i for i in range(len(ner_candidate)) if
-                 ner_candidate[i][1][0] == 'U' or ner_candidate[i][1][0] == 'L')
-    new_index = (a + b for a, b in enumerate(ner_index))
-    ner_result = extract_ner_result(ner_candidate, new_index)
-    return ner_result
 
-
-def extract_ner_result(ner_candidate, new_index):
-    new_candidate = deepcopy(ner_candidate)
-    for i in new_index:
-        new_candidate[i + 1:i + 1] = [('##split', '##split')]
-    ner_result = (
-        ' '.join(
-            [(i[0].strip() + '##' + i[1].strip()) for i in new_candidate if i[1]]).split(
-            '##split'))
-    ner_result = ([i.strip(' ') for i in ner_result if i and i != '##'])
-    ner_result = ('##'.join((' '.join([i.split('##')[0] for i in tt.split()]), tt[-3:]))
-                  for tt in
-                  ner_result)
-    ner_result = sort_dic(Counter(i for i in ner_result if i), sort_key=1, rev=True)
-    return ner_result
 
 
 def batch_loading(dict_conf, crf_f, feature_hdf, hdf_keys, switch):
@@ -104,6 +57,36 @@ def batch_loading(dict_conf, crf_f, feature_hdf, hdf_keys, switch):
                                 name_df, ticker_df, tfdf_df, tfidf_df)
     aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf = features
     return conf, crf, aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf
+
+
+def prepare_features_(dfs):
+    """
+    :param dfs: a list of pd dfs 
+    :return: a list of feature sets and feature dicts
+    """
+    feature_sets = [df2set(df) for df in dfs if len(df.columns) == 1]
+    feature_dics = [df2dic(df) for df in dfs if len(df.columns) == 2]
+    return feature_sets, feature_dics
+
+
+def batch_add_features_(data, feature_sets, feature_dics):
+    # temp = [map_set_2_matrix(chunk, f_set) for chunk in data for f_set in feature_sets]
+    temp = []
+    for f_set in feature_sets:
+        temp.append([map_set_2_matrix(chunk, f_set) for chunk in data])
+
+    temp = zip([item[-1] for ll in temp[1:] for line in ll for item in line])
+
+    result = [map_dict_2_matrix(chunk, f_dic) for chunk in temp for f_dic in feature_dics]
+    return result
+
+
+def batch_loading_(dict_conf, crf_f, feature_hdf, hdf_keys, switch):
+    conf = load_yaml_conf(dict_conf)
+    crf = jl.load(crf_f) if switch == 'test' else None
+    loads = hdf2df(feature_hdf, hdf_keys)
+    feature_sets, feature_dics = prepare_features_(loads)
+    return conf, crf, feature_sets, feature_dics
 
 
 ##############################################################################
@@ -123,8 +106,7 @@ def streaming_pos_crf(in_f, crf, conf, aca, com_single, com_suffix, location, na
                      groupby(parsed_data, lambda x: x == ('##END', '###', 'O'))
                      if not x[0]]
     test_sents = batch_add_features(prepared_data, aca, com_single, com_suffix, location,
-                                    name,
-                                    ticker, tfdf, tfidf)
+                                    name, ticker, tfdf, tfidf)
 
     X_test, y_test = feed_crf_trainer(test_sents, conf)
     crf_result = crf_predict(crf, prepared_data, X_test)
@@ -138,6 +120,28 @@ def convert_crf_result_json(crf_result, raw_df):
     raw_df = raw_df.drop(['content'], axis=1)
     json_result = raw_df.to_json(orient='records', lines=True)
     return json_result
+
+
+def pipeline_crf_train_(train_f, test_f, model_f, dict_conf, feature_hdf, hdf_keys,
+                        test_switch):
+    basic_logging('Loading begins')
+    conf, crf, feature_sets, feature_dics = batch_loading_(dict_conf, '', feature_hdf,
+                                                           hdf_keys, 'train')
+    basic_logging('Loading ends')
+    train_data, test_data = process_annotated(train_f), process_annotated(test_f)
+    train_sents = batch_add_features_(train_data, feature_sets, feature_dics)
+    test_sents = batch_add_features_(test_data, feature_sets, feature_dics)
+    basic_logging('Adding features ends')
+    X_train, y_train = feed_crf_trainer(train_sents, conf)
+    X_test, y_test = feed_crf_trainer(test_sents, conf)
+
+    basic_logging('Conversion ends')
+    crf = train_crf(X_train, y_train)
+    basic_logging('Training ends')
+    result, details = test_crf_prediction(crf, X_test, y_test, test_switch)
+    basic_logging('Testing ends')
+    # jl.dump(crf, model_f)
+    return crf, result, details
 
 
 ##############################################################################
@@ -243,8 +247,7 @@ def pipeline_crf_test(test_f, dict_conf, crf_f, feature_hdf, hdf_keys, switch,
 
 
 def pipeline_streaming_folder(in_folder, out_folder, dict_conf, crf_f, feature_hdf,
-                              hdf_keys,
-                              switch):
+                              hdf_keys, switch):
     loads = batch_loading(dict_conf, crf_f, feature_hdf, hdf_keys, switch)
     conf, crf, aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf = loads
 
@@ -253,7 +256,7 @@ def pipeline_streaming_folder(in_folder, out_folder, dict_conf, crf_f, feature_h
     for in_f in listdir(in_folder):
         ff = path.join(in_folder, in_f)
         crf_result, raw_df = streaming_pos_crf(ff, crf, conf, aca, com_single, com_suffix,
-                                        location, name, ticker, tfdf, tfidf)
+                                               location, name, ticker, tfdf, tfidf)
         # json_result = convert_crf_result_json(crf_result, raw_df)
         if modf(i / 100)[0] == 0.0:
             print(get_now(), i)
@@ -294,4 +297,3 @@ def pipeline_streaming_queue(redis_conf, dict_conf, crf_f, feature_hdf, hdf_keys
         i += 1
         if modf(i / 10)[0] == 0.0:
             print(get_now(), i)
-
