821ac6c1737fa1e4e3d5d4a731f0d37e9cbb948e
SLi <sheng.li@gemii.cc>
2017-05-09 17:54:51 +0800

    Fix process_annotated and mv back

--- code/arsenal_crf.py
+++ code/arsenal_crf.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from itertools import groupby, chain
+from itertools import chain
 from re import findall, compile
 
 import pandas as pd
 import scipy.stats as sstats
 import sklearn_crfsuite
 from sklearn.metrics import make_scorer
-# from sklearn.grid_search import RandomizedSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn_crfsuite import metrics
 
 HEADER_CRF = ['tag', 'precision', 'recall', 'f1', 'support']
-LABEL_COMPANY = ['PUB', 'EXT', 'SUB', 'PT', 'MUT', 'UMB', 'PVF', 'HOL', 'MUC', 'TRU', 'OPD', 'PEF', 'FND', 'FNS',
-                 'JVT', 'VEN', 'HED', 'UIT', 'MUE', 'ABS', 'GOV', 'ESP', 'PRO', 'FAF', 'SOV', 'COR', 'IDX', 'BAS',
-                 'PRT', 'SHP']
+LABEL_COMPANY = ['PUB', 'EXT', 'SUB', 'PT', 'MUT', 'UMB', 'PVF', 'HOL', 'MUC', 'TRU',
+                 'OPD', 'PEF', 'FND', 'FNS', 'JVT', 'VEN', 'HED', 'UIT', 'MUE', 'ABS',
+                 'GOV', 'ESP', 'PRO', 'FAF', 'SOV', 'COR', 'IDX', 'BAS', 'PRT', 'SHP']
 LABEL_COLLEGE = ['COL']
 LABEL_REMAPPED = ['ORG', 'MISC']
+HEADER_ANNOTATION = ['TOKEN' , 'POS', 'NER']
+
 
 RE_WORDS = compile(r"[\w\d\.-]+")
 
@@ -26,8 +27,29 @@ RE_WORDS = compile(r"[\w\d\.-]+")
 
 # Data preparation
 
+def process_annotated(in_file, col_names= HEADER_ANNOTATION, delimiter=('##END', '###',
+                                                                       'O')):
+    """
+    | following python-crfsuit, sklearn_crfsuit doesn't support pandas DF, so a feature 
+    | dic is used instead
+    | http://python-crfsuite.readthedocs.io/en/latest/pycrfsuite.html#pycrfsuite.ItemSequence
+    :param in_file: CSV file: TOKEN, POS, NER
+    :param col_names
+    :param delimiter
+    :return: [[sent]]
+    """
+    data = pd.read_csv(in_file, header=None, engine='c', quoting=0)
+    data.columns = col_names
+    data = data.dropna()
+    zipped_list = zip(data[i].tolist() for i in col_names)
+    sents = (tuple(i) for i in zipped_list)
+    sents = (list(x[1])[:-1] for x in groupby(sents, lambda x: x == delimiter)
+             if not x[0])
+    sents = [i for i in sents if i != []]
+    return sents
 
-def map_list_2_matrix(sent, feature_set):
+
+def map_set_2_matrix(sent, feature_set):
     """
     :param sent: [(word, pos, ner)]
     :param feature_set: {feature1, feature2}
@@ -43,7 +65,9 @@ def map_dict_2_matrix(sent, feature_dic):
     :param feature_set: {feature1:value1, feature2:value2}
     :return: [(word, pos, ner, other_features)]
     """
-    feature_list = [str(feature_dic.get(line[0].lower())) if line[0].lower() in feature_dic.keys() else '0' for line in sent]
+    feature_list = [
+        str(feature_dic.get(line[0].lower())) if line[0].lower() in feature_dic.keys()
+        else '0' for line in sent]
     return [(sent[i] + (feature_list[i],)) for i in range(len(list(sent)))]
 
 
@@ -53,81 +77,8 @@ def map_dict_2_matrix(sent, feature_dic):
 # Feature extraction
 
 
-def feature_selector(word, feature_conf, conf_switch, postag, aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf):
-    """
-    Set the feature dict here
-    :param word: word itself
-    :param feature_conf: feature config
-    :param conf_switch: select the right config from feature_config
-    :param postag:
-    :param name:
-    :param com_suffix:
-    :param country:
-    :param city:
-    :param com_single:
-    :param tfidf:
-    :param tfdf:
-    :return:
-    """
-    feature_dict = {
-        'bias': 1.0,
-        conf_switch + '_word.lower()': word.lower(),
-        conf_switch + '_word[-3]': word[-3:],
-        conf_switch + '_word[-2]': word[-2:],
-        conf_switch + '_word.isupper()': word.isupper(),
-        conf_switch + '_word.istitle()': word.istitle(),
-        conf_switch + '_word.isdigit()': word.isdigit(),
-        conf_switch + '_word.islower()': word.islower(),
-        conf_switch + '_postag': postag,
-        conf_switch + '_aca': aca,        
-        conf_switch + '_com_single': com_single,
-        conf_switch + '_com_suffix': com_suffix,
-        conf_switch + '_location': location,
-        conf_switch + '_name': name,
-        conf_switch + '_ticker': ticker,
-        conf_switch + '_tfidf': tfidf,
-        conf_switch + '_tfdf': tfdf,
-    }
-    return {i: feature_dict.get(i) for i in feature_conf[conf_switch] if i in feature_dict.keys()}
-
-
-def word2features(sent, i, feature_conf):
-
-    word, postag, _, aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf = sent[i]
-    features = feature_selector(word, feature_conf, 'current', postag, aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf)
-    if i > 0:
-        word1, postag1, _, aca1, com_single1, com_suffix1, location1, name1, ticker1, tfidf1, tfdf1 = sent[i - 1]
-        features.update(
-            feature_selector(word1, feature_conf, 'previous', postag1, aca1, com_single1, com_suffix1, location1, name1, ticker1, tfidf1, tfdf1))
-    else:
-        features['BOS'] = True
-
-    if i < len(sent) - 1:
-        word1, postag1, _, aca1, com_single1, com_suffix1, location1, name1, ticker1, tfidf1, tfdf1 = sent[i + 1]
-        features.update(
-            feature_selector(word1, feature_conf, 'next', postag1, aca1, com_single1, com_suffix1, location1, name1, ticker1, tfidf1, tfdf1))
-    else:
-        features['EOS'] = True
-
-    return features
-
-
-def sent2features(line, feature_conf):
-    return [word2features(line, i, feature_conf) for i in range(len(line))]
-
-
-def sent2labels(line):
-    return [i[2] for i in line]  # Use the correct column
-
-
-def sent2label_spfc(line, label):
-    return [i[2] if i[2].endswith(label) else '0' for i in line]
-
-
-##############################################################################
-
-
-def feature_selector_(word, feature_conf, conf_switch, postag, **features):
+def feature_selector(word, feature_conf, conf_switch, postag, aca, com_single, com_suffix,
+                     location, name, ticker, tfdf, tfidf):
     """
     Set the feature dict here
     :param word: word itself
@@ -162,40 +113,121 @@ def feature_selector_(word, feature_conf, conf_switch, postag, **features):
         conf_switch + '_tfidf': tfidf,
         conf_switch + '_tfdf': tfdf,
     }
-    return {i: feature_dict.get(i) for i in feature_conf[conf_switch] if i in feature_dict.keys()}
+    return {i: feature_dict.get(i) for i in feature_conf[conf_switch] if
+            i in feature_dict.keys()}
 
 
-def word2features_(sent, i, feature_conf, *features):
-
-    word, postag, _, features = sent[i]
-    features = feature_selector(word, feature_conf, 'current', postag)
+def word2features(sent, i, feature_conf):
+    word, postag, _, aca, com_single, com_suffix, location, name, ticker, tfdf, tfidf = \
+    sent[i]
+    features = feature_selector(word, feature_conf, 'current', postag, aca, com_single,
+                                com_suffix, location, name, ticker, tfdf, tfidf)
     if i > 0:
-        word1, postag1, _, features  = sent[i - 1]
-        features.update(feature_selector(word1, feature_conf, 'previous', postag1))
+        word1, postag1, _, aca1, com_single1, com_suffix1, location1, name1, ticker1, tfidf1, tfdf1 = \
+            sent[i - 1]
+        features.update(
+            feature_selector(word1, feature_conf, 'previous', postag1, aca1, com_single1,
+                             com_suffix1, location1, name1, ticker1, tfidf1, tfdf1))
     else:
         features['BOS'] = True
 
     if i < len(sent) - 1:
-        word1, postag1, _, features  = sent[i + 1]
-        features.update(feature_selector(word1, feature_conf, 'next', postag1))
+        word1, postag1, _, aca1, com_single1, com_suffix1, location1, name1, ticker1, tfidf1, tfdf1 = \
+            sent[i + 1]
+        features.update(
+            feature_selector(word1, feature_conf, 'next', postag1, aca1, com_single1,
+                             com_suffix1, location1, name1, ticker1, tfidf1, tfdf1))
     else:
         features['EOS'] = True
 
     return features
 
-def sent2features_(line, feature_conf, *features):
-    return [word2features_(line, i, feature_conf, *features) for i in range(len(line))]
+
+def sent2features(line, feature_conf):
+    return [word2features(line, i, feature_conf) for i in range(len(line))]
 
 
-def feed_crf_trainer_(in_data, conf, *features):
-    """
-    :param in_data:
-    :param conf_f:
-    :return:
-    """
-    features = [sent2features_(s, conf, *features) for s in in_data]
-    labels = [sent2labels(s) for s in in_data]
-    return features, labels
+def sent2labels(line):
+    return [i[2] for i in line]  # Use the correct column
+
+
+def sent2label_spfc(line, label):
+    return [i[2] if i[2].endswith(label) else '0' for i in line]
+
+
+##############################################################################
+
+
+# def feature_selector_(word, feature_conf, conf_switch, postag, **features):
+#     """
+#     Set the feature dict here
+#     :param word: word itself
+#     :param feature_conf: feature config
+#     :param conf_switch: select the right config from feature_config
+#     :param postag:
+#     :param name:
+#     :param com_suffix:
+#     :param country:
+#     :param city:
+#     :param com_single:
+#     :param tfidf:
+#     :param tfdf:
+#     :return:
+#     """
+#     feature_dict = {
+#         'bias': 1.0,
+#         conf_switch + '_word.lower()': word.lower(),
+#         conf_switch + '_word[-3]': word[-3:],
+#         conf_switch + '_word[-2]': word[-2:],
+#         conf_switch + '_word.isupper()': word.isupper(),
+#         conf_switch + '_word.istitle()': word.istitle(),
+#         conf_switch + '_word.isdigit()': word.isdigit(),
+#         conf_switch + '_word.islower()': word.islower(),
+#         conf_switch + '_postag': postag,
+#         conf_switch + '_aca': aca,
+#         conf_switch + '_com_single': com_single,
+#         conf_switch + '_com_suffix': com_suffix,
+#         conf_switch + '_location': location,
+#         conf_switch + '_name': name,
+#         conf_switch + '_ticker': ticker,
+#         conf_switch + '_tfidf': tfidf,
+#         conf_switch + '_tfdf': tfdf,
+#     }
+#     return {i: feature_dict.get(i) for i in feature_conf[conf_switch] if
+#             i in feature_dict.keys()}
+#
+#
+# def word2features_(sent, i, feature_conf, *features):
+#     word, postag, _, features = sent[i]
+#     features = feature_selector(word, feature_conf, 'current', postag)
+#     if i > 0:
+#         word1, postag1, _, features = sent[i - 1]
+#         features.update(feature_selector(word1, feature_conf, 'previous', postag1))
+#     else:
+#         features['BOS'] = True
+#
+#     if i < len(sent) - 1:
+#         word1, postag1, _, features = sent[i + 1]
+#         features.update(feature_selector(word1, feature_conf, 'next', postag1))
+#     else:
+#         features['EOS'] = True
+#
+#     return features
+#
+#
+# def sent2features_(line, feature_conf, *features):
+#     return [word2features_(line, i, feature_conf, *features) for i in range(len(line))]
+#
+#
+# def feed_crf_trainer_(in_data, conf, *features):
+#     """
+#     :param in_data:
+#     :param conf_f:
+#     :return:
+#     """
+#     features = [sent2features_(s, conf, *features) for s in in_data]
+#     labels = [sent2labels(s) for s in in_data]
+#     return features, labels
 
 
 ##############################################################################
@@ -215,7 +247,8 @@ def feed_crf_trainer(in_data, conf):
     return features, labels
 
 
-def train_crf(X_train, y_train, algm='lbfgs', c1=0.1, c2=0.1, max_iter=100, all_trans=True):
+def train_crf(X_train, y_train, algm='lbfgs', c1=0.1, c2=0.1, max_iter=100,
+              all_trans=True):
     """
     :param X_train: 
     :param y_train: 
@@ -236,9 +269,8 @@ def train_crf(X_train, y_train, algm='lbfgs', c1=0.1, c2=0.1, max_iter=100, all_
     return crf.fit(X_train, y_train)
 
 
-def show_crf_label(crf, remove_list = ['O','NER', '']):
+def show_crf_label(crf, remove_list=['O', 'NER', '']):
     labels = list(crf.classes_)
-    print(labels)
     return [i for i in labels if i not in remove_list]
 
 
@@ -269,7 +301,7 @@ def search_param(X_train, y_train, crf, params_space, f1_scorer, cv=10, iteratio
 # CRF testing and predicting
 
 
-def concert_tags(data):
+def convert_tags(data):
     converted = []
     for sent in data:
         test_result = []
@@ -280,6 +312,15 @@ def concert_tags(data):
                 test_result.append('1')
         converted.append(test_result)
     return converted
+
+
+def export_test_result(labels, y_test, y_pred):
+    details = metrics.flat_classification_report(y_test, y_pred, digits=3, labels=labels)
+    details = [i for i in [findall(RE_WORDS, i) for i in details.split('\n')] if i != []][
+              1:-1]
+    details = pd.DataFrame(details, columns=HEADER_CRF)
+    details = details.sort_values('f1', ascending=False)
+    return details
 
 
 def test_crf_prediction(crf, X_test, y_test, test_switch='spc'):
@@ -295,37 +336,51 @@ def test_crf_prediction(crf, X_test, y_test, test_switch='spc'):
 
     if test_switch == 'spc':
         labels = show_crf_label(crf)
-        result = metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
-        details = metrics.flat_classification_report(y_test, y_pred, digits=3, labels=labels)
-        details = [i for i in [findall(RE_WORDS, i) for i in details.split('\n')] if i != []][1:-1]
-        details = pd.DataFrame(details, columns=HEADER_CRF)
-        details = details.sort_values('f1', ascending=False)
 
+        result = metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
+        details = export_test_result(labels, y_test, y_pred)
         return result, details
 
     elif test_switch == 'bin':
 
-        y_pred_converted = concert_tags(y_pred)
-        y_test_converted = concert_tags(y_test)
+        y_pred_converted = convert_tags(y_pred)
+        y_test_converted = convert_tags(y_test)
+        labels = ['1']
 
-        result = metrics.flat_f1_score(y_test_converted, y_pred_converted, average='weighted', labels=['1'])
-
-        y_test_converted = ['0' if j == 'O' else '1' for i in y_test for j in i]
-        details = metrics.flat_classification_report(y_test_converted, y_pred_converted, digits=3, labels=['1'])
-
-        details = [i for i in [findall(RE_WORDS, i) for i in details.split('\n')] if i != []][1:-1]
-        details = pd.DataFrame(details, columns=HEADER_CRF)
-        details = details.sort_values('f1', ascending=False)
-
+        result = metrics.flat_f1_score(y_test_converted, y_pred_converted,
+                                       average='weighted',
+                                       labels=labels)
+        y_test_flatten = ['0' if j == 'O' else '1' for i in y_test for j in i]
+        details = export_test_result(labels, y_test_flatten, y_pred_converted)
         return result, details
 
 
+# def test_crf(crf, y_pred, y_test, test_switch):
+#     if test_switch == 'spc':
+#         labels = show_crf_label(crf)
+#         show_crf_label(crf)
+#         result = metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
+#
+#     elif test_switch == 'bin':
+#         labels = ['1']
+#         y_pred = convert_tags(y_pred)
+#         y_test = convert_tags(y_test)
+#
+#         result = metrics.flat_f1_score(y_test, y_pred, average='weighted', labels=labels)
+#         y_test = ['0' if j == 'O' else '1' for i in y_test for j in i]
+#
+#     details = metrics.flat_classification_report(y_test, y_pred, digits=3, labels=labels)
+#     details = [i for i in [findall(RE_WORDS, i) for i in details.split('\n')] if i != []][1:-1]
+#     details = pd.DataFrame(details, columns=HEADER_CRF)
+#     details = details.sort_values('f1', ascending=False)
+#     return details, result
 
 
 def crf_predict(crf, new_data, processed_data):
     result = crf.predict(processed_data)
     length = len(list(new_data))
-    crf_result = ([(new_data[j][i][:2] + (result[j][i],)) for i in range(len(new_data[j]))] for j in
-                  range(length))
+    crf_result = (
+    [(new_data[j][i][:2] + (result[j][i],)) for i in range(len(new_data[j]))] for j in
+    range(length))
     crf_result = [i + [('##END', '###', 'O')] for i in crf_result]
     return list(chain.from_iterable(crf_result))
