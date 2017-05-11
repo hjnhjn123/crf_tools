b7b5e5b61a0f63b7833bdb1335c9aca94a81f903
SLi <sheng.li@gemii.cc>
2017-05-11 16:25:18 +0800

    Fix batch_add_features_, batch_loading_, pipeline_crf_train_

--- code/pipeline_crf.py
+++ code/pipeline_crf.py
@@ -65,9 +65,9 @@ def prepare_features_(dfs):
     :param dfs: a list of pd dfs 
     :return: a list of feature sets and feature dicts
     """
-    feature_sets = [df2set(df) for df in dfs if len(df.columns) == 1]
-    feature_dics = [df2dic(df) for df in dfs if len(df.columns) == 2]
-    return feature_sets, feature_dics
+    f_sets = [df2set(df) for df in dfs if len(df.columns) == 1]
+    f_dics = [df2dic(df) for df in dfs if len(df.columns) == 2]
+    return f_sets, f_dics
 
 
 def batch_loading_(dict_conf, crf_f, feature_hdf, hdf_keys, crf_model=False):
@@ -83,29 +83,29 @@ def batch_loading_(dict_conf, crf_f, feature_hdf, hdf_keys, crf_model=False):
     conf = load_yaml_conf(dict_conf)
     crf = jl.load(crf_f) if crf_model else None
     loads = hdf2df(feature_hdf, hdf_keys)
-    feature_sets, feature_dics = prepare_features_(loads)
-    return conf, crf, feature_sets, feature_dics
+    f_sets, f_dics = prepare_features_(loads)
+    return conf, crf, f_sets, f_dics
 
 
-def batch_add_features_(data, feature_sets, feature_dics):
-    # temp = [map_set_2_matrix(chunk, f_set) for chunk in data for f_set in feature_sets]
-    temp = []
-    for f_set in feature_sets:
-        temp.append([map_set_2_matrix(chunk, f_set) for chunk in data])
-
-    temp = zip([item[-1] for ll in temp[1:] for line in ll for item in line])
-
-    result = [map_dict_2_matrix(chunk, f_dic) for chunk in temp for f_dic in feature_dics]
-    return result
+def batch_add_features_(df, f_sets, f_dics):
+    cols = df.columns
+    set_name = [str(i) for i in range(len(f_sets))]
+    dic_name = [str(-i-1) for i in range(len(f_dics))]
+    df_list0 = [map_set2df(df, c_name, f_set) for c_name, f_set in zip(set_name, f_sets)]
+    basic_logging('mapping set ends')
+    df_list1 = [map_dic2df(df, c_name, f_dic) for c_name, f_dic in zip(dic_name, f_dics)]
+    basic_logging('mapping dic ends')
+    return df_list1[-1]
 
 
 def pipeline_crf_train_(train_f, test_f, model_f, dict_conf, f_hdf, hdf_key, report_type):
     basic_logging('Loading begins')
     conf, crf, f_sets, f_dics = batch_loading_(dict_conf, '', f_hdf, hdf_key)
     basic_logging('Loading ends')
-    train_data, test_data = process_annotated(train_f), process_annotated(test_f)
-    train_sents = batch_add_features_(train_data, f_sets, f_dics)
-    test_sents = batch_add_features_(test_data, f_sets, f_dics)
+    train_df, test_df = process_annotated_(train_f), process_annotated_(test_f)
+
+    train_sents = batch_add_features_(train_df, f_sets, f_dics)
+    test_sents = batch_add_features_(test_df, f_sets, f_dics)
     basic_logging('Adding features ends')
     X_train, y_train = feed_crf_trainer(train_sents, conf)
     X_test, y_test = feed_crf_trainer(test_sents, conf)
