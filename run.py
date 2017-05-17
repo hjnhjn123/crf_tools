7ef3c3c6d60fec714fe8becf9963fda97ed9d863
SLi <sheng.li@gemii.cc>
2017-05-17 16:47:05 +0800

    Change code dir to src dir

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from sys import argv
 import os
-from code.pipeline_crf import main
+from src.pipeline_crf import main
 
 if __name__ == '__main__':
     # os.environ['PROJECT_DIR'] = os.path.dirname(os.path.abspath(__file__))
