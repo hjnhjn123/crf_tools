307202d2a269122f32993db021823d7aa6ddf37e
SLi <sheng.li@gemii.cc>
2017-05-17 15:05:37 +0800

    Add run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+from sys import argv
+import os
+from code.pipeline_crf import main
+
+if __name__ == '__main__':
+    os.environ['PROJECT_DIR'] = os.path.dirname(os.path.abspath(__file__))
+    main(argv)
