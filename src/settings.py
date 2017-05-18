576b5d2fa074bde48fc9dd472f9dd4fd9b68a66a
SLi <sheng.li@gemii.cc>
2017-05-18 10:58:56 +0800

    Add guidance in settings

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,8 @@
 
 # FEATURES
 
+# Set features to compute
+
 FEATURE_FUNCTION = {
     'current_original': lambda x: x,
     'current_lower': lambda x: x.lower(),
@@ -29,30 +31,39 @@ FEATURE_FUNCTION = {
 
 # FILE DIR
 
+# Set train DIR
 TRAIN_F = '/Users/acepor/Work/patsnap/data/pat360ner_data/annotated_data' \
     '/annotate_train_20170425.csv'
 
+# Set test DIR
 TEST_F = '/Users/acepor/Work/patsnap/data/pat360ner_data/annotated_data' \
     '/annotate_test_20170425.csv'
 
+# Set validate DIR
 VALIDATE_F = '/Users/acepor/Work/patsnap/data/pat360ner_data/annotated_data' \
     '/zdnet_test_20170425.csv'
 
+# Set HDF5 DIR
 HDF_F = '/Users/acepor/Work/patsnap/data/pat360ner_data/dicts/features_20170425.h5'
 
+# Set model DIR
 MODEL_F = ''
 
+# Set output DIR
 OUT_F = ''
 
 ##############################################################################
 
 # MISC
 
-
+# Set HDF5 key to extract feature dicts
 HDF_KEY = ['aca', 'com_single', 'com_suffix', 'location', 'name', 'ticker', 'tfidf']
 
+# Set report type
 REPORT_TYPE = 'spc'
 
+# Set cv scale
 CV = 5
 
+# Set iteration
 ITERATION = 10
