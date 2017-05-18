2879920fc5805ce8851c7508928065fcca79ca07
SLi <sheng.li@gemii.cc>
2017-05-18 15:40:52 +0800

    Cleanup

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ import os
 from src.pipeline_crf import main
 
 if __name__ == '__main__':
-    # os.environ['PROJECT_DIR'] = os.path.dirname(os.path.abspath(__file__))
-    main(argv)
+    main(argv[1])
