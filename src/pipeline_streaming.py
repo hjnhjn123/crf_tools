88e1587876414891a6a697041eb4fff1d1dd20fb
SLi <sheng.li@gemii.cc>
2017-05-31 15:36:15 +0800

    Fix streaming_pos_crf, pipeline_streaming_sqs

--- src/pipeline_streaming.py
+++ src/pipeline_streaming.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from itertools import chain, groupby
+from itertools import chain
 
 import pandas as pd
 
@@ -14,9 +14,9 @@ from .settings import *
 ##############################################################################
 
 
-def streaming_pos_crf(in_f, crf, f_dics, feature_conf, hdf_key, window_size):
+def streaming_pos_crf(in_f, crf, f_dics, feature_conf, hdf_key, window_size, col):
     raw_df = pd.read_json(in_f, lines=True)
-    raw_df['content'] = raw_df.DETAIL.to_dict()[0]['content']
+    raw_df['content'] = raw_df[col].to_dict()[0]['content']
 
     parsed_data = chain.from_iterable(spacy_batch_processing(raw_df, '', 'content', ['content'], 'crf'))
     prepared_data = pd.DataFrame(list(parsed_data))
@@ -30,15 +30,15 @@ def streaming_pos_crf(in_f, crf, f_dics, feature_conf, hdf_key, window_size):
 ##############################################################################
 
 
-def pipeline_streaming_sqs(in_queue, out_queue, model_f, hdf_f, hdf_key, feature_conf, window_size, online):
+def pipeline_streaming_sqs(in_queue, out_queue, model_f, hdf_f, hdf_key, feature_conf, window_size, col, online):
     sqs_queues = sqs_get_msgs(in_queue, online)
     crf, f_dics = batch_loading(model_f, hdf_f, hdf_key)
 
     while True:
         for q in sqs_queues.receive_messages():
             json_input = q.body
-            crf_result, raw_df = streaming_pos_crf(json_input, crf, f_dics, feature_conf, hdf_key, window_size)
-            json_result = crf_result2json(crf_result, raw_df)
+            crf_result, raw_df = streaming_pos_crf(json_input, crf, f_dics, feature_conf, hdf_key, window_size, col)
+            json_result = crf_result2json(crf_result, raw_df, col)
             sqs_send_msg(json_result, queue_name=out_queue)
             basic_logging('Queue output')
             # q.delete()
@@ -52,4 +52,5 @@ def main(argv):
         s3_get_file(S3_BUCKET, MODEL_KEY, MODEL_FILE, online)
         s3_get_file(S3_BUCKET, HDF_FILE_KEY, HDF_FILE, online)
         basic_logging('Queue prepared')
-        pipeline_streaming_sqs(IN_QUEUE, OUT_QUQUE, MODEL_FILE, HDF_FILE, HDF_KEY, FEATURE_CONF, WINDOW_SIZE, online)
+        pipeline_streaming_sqs(IN_QUEUE, OUT_QUQUE, MODEL_FILE, HDF_FILE, HDF_KEY, FEATURE_CONF, WINDOW_SIZE,
+                               CONTENT_COL, online)
