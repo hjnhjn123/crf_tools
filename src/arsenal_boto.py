0ca8022c5dbb8465275f1c757ee8e581f64ffff6
SLi <sheng.li@gemii.cc>
2017-05-31 10:14:10 +0800

    add online / offline options

--- src/arsenal_boto.py
+++ src/arsenal_boto.py
@@ -2,13 +2,13 @@
 
 import boto3
 
-def sqs_send_a_msg(data, queue_name='360_nlp_output'):
-    sqs = boto3.resource('sqs')
+def sqs_send_a_msg(data, queue_name='360_nlp_output', online=True):
+    sqs = boto3.resource('sqs') if online else boto3.resource('sqs', endpoint_url='http://192.168.27.190:4576')
     queue = sqs.get_queue_by_name(QueueName=queue_name)
     queue.send_message(MessageBody=data)
 
 
-def sqs_get_msgs(queue_name='360_nlp_input'):
-    sqs = boto3.resource('sqs')
-    queues = sqs.get_queue_by_name(QueueName=queue_name)
+def sqs_get_msgs(queue_name='360_nlp_input', online=True):
+    sqs = boto3.resource('sqs') if online else boto3.resource('sqs', endpoint_url='http://192.168.27.190:4576')
+    queues = sqs.get_queue_by_name(QueueName=queue_name, )
     return queues
