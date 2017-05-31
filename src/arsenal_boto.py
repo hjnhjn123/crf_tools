0cba02a086b8c80d22d4ab9f057a02ab827f3f93
SLi <sheng.li@gemii.cc>
2017-05-31 14:38:29 +0800

    Fix sqs_send_msg, sqs_get_msgs, s3_get_file

--- src/arsenal_boto.py
+++ src/arsenal_boto.py
@@ -3,19 +3,21 @@
 import boto3
 
 
-def sqs_send_msg(data, queue_name='360_nlp_output', online=True):
+def sqs_send_msg(data, queue_name='360_nlp_output', online='aws'):
     sqs = boto3.resource('sqs') if online else boto3.resource('sqs', endpoint_url='http://192.168.27.190:4576')
     queue = sqs.get_queue_by_name(QueueName=queue_name)
     queue.send_message(MessageBody=data)
 
 
-def sqs_get_msgs(queue_name='360_nlp_input', online=True):
+def sqs_get_msgs(queue_name='360_nlp_input', online='aws'):
     sqs = boto3.resource('sqs') if online else boto3.resource('sqs', endpoint_url='http://192.168.27.190:4576')
     queues = sqs.get_queue_by_name(QueueName=queue_name, )
     return queues
 
 
-def s3_get_file(bucket, key, file, online=True):
-    s3 = boto3.client('s3') if online else boto3.resource('s3', endpoint_url='http://192.168.27.190:4572')
-    file = s3.download_file(bucket, key, file)
-    return file
+
+def s3_get_file(bucket, key, file, online='aws'):
+    s3 = boto3.resource('s3') if online=='aws' else boto3.resource('s3', endpoint_url='http://192.168.27.190:4572')
+    bucket = s3.Bucket(bucket)
+    with open(file, 'wb') as data:
+        bucket.download_fileobj(key, data)
