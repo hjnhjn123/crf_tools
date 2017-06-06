a932d051de9ff8b0afb9ffe19d45f41743713a59
SLi <sheng.li@gemii.cc>
2017-06-06 14:10:12 +0800

    Migrate arsenal_crf from 360-nlp

--- src/pipeline_crf.py
+++ src/pipeline_crf.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from .arsenal_crf import process_annotated, batch_add_features, batch_loading, feed_crf_trainer, test_crf_prediction, \
-    df2crfsuite, train_crf, show_crf_label, make_param_space, make_f1_scorer
+from .arsenal_crf import process_annotated, batch_add_features, batch_loading, feed_crf_trainer, df2crfsuite, train_crf, \
+    make_param_space, make_f1_scorer, search_param
 from .arsenal_logging import basic_logging
-from .arsenal_test import test_crf_prediction
+from .arsenal_test import show_crf_label, evaluate_ner_result
 from .settings import *
+from .arsenal_stats import get_now
+import joblib as jl
 
 
 ##############################################################################
@@ -42,14 +44,11 @@ def pipeline_train(train_f, test_f, model_f, result_f, hdf_f, hdf_key, feature_c
     basic_logging('training ends')
     y_pred = crf.predict(X_test)
     result = evaluate_ner_result([i for j in y_pred for i in j], [i for j in y_test for i in j])
-
-    # overall_f1, details = test_crf_prediction(crf, y_pred, y_test, report_type)
-    # details['overall_f1'] = overall_f1
     # result.to_csv(result_f, index=False)
     basic_logging('testing ends')
     if model_f:
         jl.dump(crf, model_f)
-    return crf, details
+    return crf, result
 
 
 def pipeline_best_predict(train_f, test_f, model_f, result_f, feature_conf, hdf_f, hdf_key, report_type, cv, iteration,
@@ -89,13 +88,12 @@ def pipeline_best_predict(train_f, test_f, model_f, result_f, feature_conf, hdf_
     basic_logging('cv ends')
     best_predictor = rs_cv.best_estimator_
     y_pred = crf.predict(X_test)
-    best_f1, best_details = test_crf_prediction(best_predictor, y_pred, y_test, report_type)
-    basic_logging('Testing ends')
-    best_details['overall_f1'] = best_f1
-    best_details.to_csv(result_f, index=False)
+    result = evaluate_ner_result([i for j in y_pred for i in j], [i for j in y_test for i in j])
+    # result.to_csv(result_f, index=False)
+    basic_logging('testing ends')
     if model_f:
         jl.dump(best_predictor, model_f)
-    return crf, best_predictor, rs_cv, best_details
+    return crf, best_predictor, rs_cv, result
 
 
 def pipeline_validate(validate_f, model_f, feature_conf, hdf_f, result_f, hdf_key, report_type, window_size):
@@ -122,10 +120,8 @@ def pipeline_validate(validate_f, model_f, feature_conf, hdf_f, result_f, hdf_ke
     y_pred = crf.predict(X_test)
     result = evaluate_ner_result([i for j in y_pred for i in j], [i for j in y_test for i in j])
 
-    overall_f1, details = test_crf_prediction(crf, y_pred, y_test, report_type)
-    details['overall_f1'] = overall_f1
-    details.to_csv(result_f, index=False)
-    return result, details
+    # result.to_csv(result_f, index=False)
+    return result
 
 
 ##############################################################################
@@ -140,21 +136,21 @@ def main(argv):
     dic = {
         'train': lambda: pipeline_train(train_f=TRAIN_F, test_f=TEST_F, model_f=MODEL_F,
                                         result_f=RESULT_F, hdf_f=HDF_F, hdf_key=HDF_KEY,
-                                        feature_conf=FEATURE_FUNCTION,
+                                        feature_conf=FEATURE_CONF,
                                         report_type=REPORT_TYPE,
                                         window_size=WINDOW_SIZE),
         'cv': lambda: pipeline_best_predict(train_f=TRAIN_F, test_f=TEST_F,
                                             model_f=MODEL_F,
                                             result_f=RESULT_F, hdf_f=HDF_F,
                                             hdf_key=HDF_KEY,
-                                            feature_conf=FEATURE_FUNCTION,
+                                            feature_conf=FEATURE_CONF,
                                             report_type=REPORT_TYPE,
                                             window_size=WINDOW_SIZE, cv=CV,
                                             iteration=ITERATION),
         'validate': lambda: pipeline_validate(validate_f=VALIDATE_F, model_f=MODEL_F,
                                               result_f=RESULT_F, hdf_f=HDF_F,
                                               hdf_key=HDF_KEY,
-                                              feature_conf=FEATURE_FUNCTION,
+                                              feature_conf=FEATURE_CONF,
                                               report_type=REPORT_TYPE,
                                               window_size=WINDOW_SIZE)
     }
