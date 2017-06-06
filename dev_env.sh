a802b028d5c6f6b5ca418102dfb16a5eb77d3c7a
SLi <sheng.li@gemii.cc>
2017-06-06 14:13:55 +0800

    Migrate dev_env from 360-nlp

--- dev_env.sh
+++ dev_env.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 export METRICS_URL=http://10.10.130.103:86
 
 export NLP_S3_BUCKET=patsnap-360-npl
 export NLP_SQS_QUEUE_INPUT=360_nlp_input
 export NLP_SQS_QUEUE_OUTPUT=360_nlp_output
 
-export NLP_MODEL_KEY=360-nlp/ner-models/crf_en_model_20170425_bp.joblib
+export NLP_MODEL_KEY=360-nlp/ner-models/crf_en_model_20170602_bp.joblib
 export NLP_HDF_FILE_KEY=360-nlp/ner-dicts/features_20170425.h5
 
 export NLP_MODE=dev
