9348b1df4bf455e58f496f8d84754119738f9509
SLi <sheng.li@gemii.cc>
2017-06-20 09:30:00 +0800

    Add pipeline_best_predict_mix

--- src/pipeline_crf.py
+++ src/pipeline_crf.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import joblib as jl
+import pandas as pd
+
 from .arsenal_crf import process_annotated, batch_add_features, batch_loading, feed_crf_trainer, df2crfsuite, train_crf, \
     make_param_space, make_f1_scorer, search_param, merge_ner_tags
 from .arsenal_logging import basic_logging
+from .arsenal_stats import get_now
 from .arsenal_test import show_crf_label, evaluate_ner_result
 from .settings import *
-from .arsenal_stats import get_now
-import joblib as jl
 
 
 ##############################################################################
@@ -42,7 +44,7 @@ def pipeline_train(train_f, test_f, model_f, result_f, hdf_f, hdf_key, feature_c
     crf = train_crf(X_train, y_train)
     basic_logging('training ends')
     y_pred = crf.predict(X_test)
-    result = evaluate_ner_result(y_pred, y_test)
+    result, indexed_ner = evaluate_ner_result(y_pred, y_test)
     result.to_csv(result_f, index=False)
     basic_logging('testing ends')
     if model_f:
@@ -50,7 +52,7 @@ def pipeline_train(train_f, test_f, model_f, result_f, hdf_f, hdf_key, feature_c
     return crf, result
 
 
-def pipeline_train_mix(test_f, model_f, result_f, hdf_f, hdf_key, feature_conf, window_size, ner_tag, *train_fs):
+def pipeline_train_mix(test_f, model_f, result_f, hdf_f, hdf_key, feature_conf, window_size, ner_tags, *train_fs):
     """
     A pipeline for CRF training
     :param train_fs: train dataset in a 3-column csv (TOKEN, POS, LABEL)
@@ -68,8 +70,8 @@ def pipeline_train_mix(test_f, model_f, result_f, hdf_f, hdf_key, feature_conf, 
     test_df = process_annotated(test_f)
     basic_logging('loading data ends')
     if ner_tags:
-        train_df = merge_ner_tags(train_df, 'NER', ner_tag)
-        test_df = merge_ner_tags(test_df, 'NER', ner_tag)
+        train_df = merge_ner_tags(train_df, 'NER', ner_tags)
+        test_df = merge_ner_tags(test_df, 'NER', ner_tags)
     train_df = batch_add_features(train_df, f_dics)
     test_df = batch_add_features(test_df, f_dics)
     basic_logging('adding features ends')
@@ -82,7 +84,7 @@ def pipeline_train_mix(test_f, model_f, result_f, hdf_f, hdf_key, feature_conf, 
     crf = train_crf(X_train, y_train)
     basic_logging('training ends')
     y_pred = crf.predict(X_test)
-    result = evaluate_ner_result(y_pred, y_test)
+    result, indexed_ner = evaluate_ner_result(y_pred, y_test)
     result.to_csv(result_f, index=False)
     basic_logging('testing ends')
     if model_f:
@@ -126,7 +128,56 @@ def pipeline_best_predict(train_f, test_f, model_f, result_f, feature_conf, hdf_
     basic_logging('cv ends')
     best_predictor = rs_cv.best_estimator_
     y_pred = crf.predict(X_test)
-    result = evaluate_ner_result(y_pred, y_test)
+    result, indexed_ner = evaluate_ner_result(y_pred, y_test)
+    # result.to_csv(result_f, index=False)
+    basic_logging('testing ends')
+    if model_f:
+        jl.dump(best_predictor, model_f)
+    return crf, best_predictor, rs_cv, result
+
+
+def pipeline_best_predict_mix(test_f, model_f, result_f, feature_conf, hdf_f, hdf_key, cv, iteration, window_size,
+                              ner_tags, *train_fs):
+    """
+    A pipeline for CRF training
+    :param train_f: train dataset in a 3-column csv (TOKEN, POS, LABEL)
+    :param test_f: test dataset in a 3-column csv (TOKEN, POS, LABEL)
+    :param model_f: model file
+    :param feature_conf: feature configurations
+    :param hdf_f: feature HDF5 file
+    :param hdf_key: keys of feature HDF5 file
+    :param cv: cv scale
+    :param iteration: iteration time
+    """
+    basic_logging('loading conf begins')
+    _, f_dics = batch_loading('', hdf_f, hdf_key)
+    basic_logging('loading conf ends')
+    train_df = pd.concat([process_annotated(f) for f in train_fs])
+    print(train_df.info())
+    test_df = process_annotated(test_f)
+    basic_logging('loading data ends')
+    if ner_tags:
+        train_df = merge_ner_tags(train_df, 'NER', ner_tags)
+        test_df = merge_ner_tags(test_df, 'NER', ner_tags)
+    train_df = batch_add_features(train_df, f_dics)
+    test_df = batch_add_features(test_df, f_dics)
+    basic_logging('adding features ends')
+    train_sents = df2crfsuite(train_df)
+    test_sents = df2crfsuite(test_df)
+    basic_logging('converting to crfsuite ends')
+    X_train, y_train = feed_crf_trainer(train_sents, feature_conf, hdf_key, window_size)
+    X_test, y_test = feed_crf_trainer(test_sents, feature_conf, hdf_key, window_size)
+    basic_logging('Conversion ends')
+    crf = train_crf(X_train, y_train)
+    labels = show_crf_label(crf)
+    params_space = make_param_space()
+    f1_scorer = make_f1_scorer(labels)
+    basic_logging('cv begins')
+    rs_cv = search_param(X_train, y_train, crf, params_space, f1_scorer, cv, iteration)
+    basic_logging('cv ends')
+    best_predictor = rs_cv.best_estimator_
+    y_pred = crf.predict(X_test)
+    result, indexed_ner = evaluate_ner_result(y_pred, y_test)
     # result.to_csv(result_f, index=False)
     basic_logging('testing ends')
     if model_f:
@@ -158,7 +209,7 @@ def pipeline_validate(valid_f, model_f, feature_conf, hdf_f, result_f, hdf_key, 
     X_test, y_test = feed_crf_trainer(test_sents, feature_conf, hdf_key, window_size)
     basic_logging('Conversion ends')
     y_pred = crf.predict(X_test)
-    result = evaluate_ner_result(y_pred, y_test)
+    result, indexed_ner = evaluate_ner_result(y_pred, y_test)
     result.to_csv(result_f, index=False)
     return result
 
