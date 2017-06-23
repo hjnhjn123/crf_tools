77272b75ec4f2a4495d46a29d582d81a912bb519
SLi <sheng.li@gemii.cc>
2017-06-23 16:34:38 +0800

    Add voting

--- src/pipeline_streaming.py
+++ src/pipeline_streaming.py
@@ -3,11 +3,13 @@
 from itertools import chain
 
 import pandas as pd
+import joblib as jl
 
 from .arsenal_boto import sqs_get_msgs, sqs_send_msg, s3_get_file
 from .arsenal_crf import batch_add_features, batch_loading, feed_crf_trainer, crf_predict, crf_result2json, df2crfsuite
 from .arsenal_logging import basic_logging
 from .arsenal_spacy import spacy_batch_processing
+from .arsenal_stats import combine_multi_df
 from .settings import *
 
 
@@ -26,7 +28,7 @@ def streaming_pos_crf(in_f, crf, f_dics, feature_conf, hdf_key, window_size, col
     return crf_result, raw_df
 
 
-def streaming_pos_crf_multi(in_f, crf1, crf2, f_dics, feature_conf, hdf_key, window_size, col):
+def streaming_pos_crf_multi(in_f, f_dics, feature_conf, hdf_key, window_size, col, models):
     raw_df = pd.read_json(in_f, lines=True)
     raw_df['content'] = raw_df[col].to_dict()[0]['content']
     parsed_data = chain.from_iterable(spacy_batch_processing(raw_df, '', 'content', ['content'], 'crf'))
@@ -34,43 +36,60 @@ def streaming_pos_crf_multi(in_f, crf1, crf2, f_dics, feature_conf, hdf_key, win
     test_df = batch_add_features(prepared_data, f_dics)
     test_sents = df2crfsuite(test_df)
     X_test, y_test = feed_crf_trainer(test_sents, feature_conf, hdf_key, window_size)
-    crf_result1 = crf_predict(crf1, test_sents, X_test)
-    crf_result2 = crf_predict(crf2, test_sents, X_test)
+    crf_results = [crf_predict(model, test_sents, X_test) for model in models]
+    return crf_results, raw_df
 
-    return crf_result, raw_df
+
+def voting(crf_results):
+    crf_dfs = [pd.DataFrame(crf_list, columns=HEADER) for crf_list in crf_results]
+    combined = pd.concat(crf_dfs, axis=1)
+    combined.columns = range(len(combined.columns))
+    to_vote = combined[[i for i in combined.columns if (i + 1) % 3 == 0]].apply(tuple, axis=1).tolist()
+    # convert a df to zipped list
+
+    vote_result = []
+    for i in to_vote:
+        if i == tuple('O' for k in range(len(i))):
+            vote_result.append('O')
+            # if all items are 'O', just append it
+        for j in range(len(i)):
+            if i[j] != 'O':
+                vote_result.append(i[j])
+                break
+            elif i[j] == 'O':
+                j += 1
+    return list(zip(combined[0].tolist(), combined[1].tolist(), vote_result))
+
 
 ##############################################################################
 
 
 def pipeline_streaming_sqs(in_queue, out_queue, model_f, hdf_f, hdf_key, feature_conf, window_size, col):
     sqs_queues = sqs_get_msgs(in_queue)
-    crf, f_dics = batch_loading(model_f, hdf_f, hdf_key)
+    model = jl.load(model_f)
+    f_dics = batch_loading(hdf_f, hdf_key)
 
     while True:
         for q in sqs_queues.receive_messages():
             json_input = q.body
-            crf_result, raw_df = streaming_pos_crf(json_input, crf, f_dics, feature_conf, hdf_key, window_size, col)
+            crf_result, raw_df = streaming_pos_crf(json_input, model, f_dics, feature_conf, hdf_key, window_size, col)
             json_result = crf_result2json(crf_result, raw_df, col)
             sqs_send_msg(json_result, queue_name=out_queue)
             basic_logging('Queue output')
             q.delete()
 
 
-def pipeline_multi_streaming_sqs(in_queue, out_queue, model_f1, model_f2, hdf_f, hdf_key, feature_conf, window_size,
-                                 col):
+def pipeline_multi_streaming_sqs(in_queue, out_queue, hdf_f, hdf_key, feature_conf, window_size, col, *model_fs):
     sqs_queues = sqs_get_msgs(in_queue)
-    crf1, f_dics = batch_loading(model_f1, hdf_f, hdf_key)
-    crf2, _ = batch_loading(model_f2, hdf_f, hdf_key)
-
+    models = [jl.load(model) for model in model_fs]
+    f_dics = batch_loading(hdf_f, hdf_key)
 
     while True:
         for q in sqs_queues.receive_messages():
             json_input = q.body
-            crf_result1, raw_df = streaming_pos_crf(json_input, crf1, f_dics, feature_conf, hdf_key, window_size,
-                                                   col)
-            crf_result2, raw_df = streaming_pos_crf(json_input, crf2, f_dics, feature_conf, hdf_key, window_size,
-                                                   col)
-            json_result = crf_result2json(crf_result1, raw_df, col)
+            crf_results, raw_df = streaming_pos_crf_multi(json_input, f_dics, feature_conf, hdf_key, window_size,col, models)
+            final_result = voting(crf_results)
+            json_result = crf_result2json(final_result, raw_df, col)
             sqs_send_msg(json_result, queue_name=out_queue)
             basic_logging('Queue output')
             q.delete()
